5684728c2e06182f3a60246bd09764d341109083
China-Htf <837183807@qq.com>
2021-12-21 20:36:10 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    App
+    {{$store.state.name}}
   </div>
-  <router-view/>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup>
 
-#nav {
-  padding: 30px;
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style lang="less" scoped>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import { createStore } from 'vuex'
 
+  // 创建vuex仓库并导出
 export default createStore({
+  // 数据
   state: {
+    name: 'zhangsan'
   },
+  // vuex的计算属性
+  getters: {
+  },
+  // 改数据函数
   mutations: {
   },
+  // 请求数据函数
   actions: {
   },
+  // 分模块
   modules: {
   }
 })
